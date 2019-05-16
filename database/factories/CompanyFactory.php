d57f54447a5b6ce2ed0c17add5ff625e0eb79912
pascal de laroche <pascal.dls@gmail.com>
2019-05-16 14:52:01 +0400

    Factory

--- database/factories/CompanyFactory.php
+++ database/factories/CompanyFactory.php
@@ -2,8 +2,12 @@
 
 use Faker\Generator as Faker;
 
+// Company::all()->pluck('name')
+// factory(\App\Company::class, 10 )->create()
+
 $factory->define(App\Company::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->company,
+        'phone' => $faker->phoneNumber
     ];
 });
