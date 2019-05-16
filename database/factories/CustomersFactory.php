5ddb76a5a29d21a7ba3c0758f0ad32c64f11d4cd
pascal de laroche <pascal.dls@gmail.com>
2019-05-16 15:05:51 +0400

    seeders with factory

--- database/factories/CustomersFactory.php
+++ database/factories/CustomersFactory.php
@@ -1,9 +1,13 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\Company;
 
 $factory->define(App\Customers::class, function (Faker $faker) {
     return [
-        //
+        'company_id' =>  factory(Company::class)->create()->id,
+        'name' =>  $faker->name(),
+        'email' => $faker->unique()->safeEmail(),
+        'active' => 0
     ];
 });
---
+++ database/seeds/CompanyTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CompanyTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+
+        factory(\App\Company::class, 5)->create();
+    }
+}
