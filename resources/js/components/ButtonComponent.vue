4e24f662986b320e64a49fb4a598cb25d60e8bf6
pascal de laroche <pascal.dls@gmail.com>
2019-05-16 09:29:08 +0400

    vue components

---
+++ resources/js/components/ButtonComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <button class="btn btn-secondary" :type="type" v-text="text"></button>
+    <p v-text="test"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["text", "type"],
+  data: function() {
+    return {
+      test: ""
+    };
+  },
+  mounted() {
+    console.log("Component mounted.");
+    axios
+      .get("api/vue", {})
+      .then(result => {
+        console.log(result);
+        this.test = result.data;
+      })
+      .catch(err => {});
+  }
+};
+</script>
+
+<style>
+.my-button {
+  background-color: 333;
+}
+</style>
+
